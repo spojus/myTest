5ee437207a842b97580e6e30aaa028cae6cb73e4
spojus <xieguoxi@gmail.com>
2015-07-06 00:47:04 +0800

    use "/" for DIRECTORY_SEPARATOR

--- data.php
+++ data.php
@@ -1,31 +1,30 @@
 <?php
 //path
-define('ROOT_PATH',	'.'.DIRECTORY_SEPARATOR);
-define('SRC_PATH',	ROOT_PATH.'src'.DIRECTORY_SEPARATOR);
+define('ROOT_PATH', './');
+define('SRC_PATH', ROOT_PATH . 'src/');
+define('CTRL_PATH', SRC_PATH . 'controller/');
 
 //result code
 define('CODE_OK', 0);
 define('CODE_FAIL', -1);
 
-include_once(SRC_PATH.DIRECTORY_SEPARATOR.'common.function.php');
-include_once(SRC_PATH.DIRECTORY_SEPARATOR.'common.config.php');
+include(SRC_PATH . 'common.function.php');
+include(SRC_PATH . 'common.config.php');
 //get post code
-
 
 
 //get file code
 
 
-
-if(!(isset($_GET['C']) && isset($_GET['A']))) {
-	showMsg('no C or A');
+if (!(isset($_GET['C']) && isset($_GET['A']))) {
+    showMsg('no C or A');
 }
 
 $CONTROLLER = $_GET['C'];
 $ACTION = $_GET['A'];
 
-if(strlen($CONTROLLER)) {
-	include_once(SRC_PATH.'controller'.DIRECTORY_SEPARATOR.$CONTROLLER.'.class.php');
+if (strlen($CONTROLLER)) {
+    include(CTRL_PATH . $CONTROLLER . '.class.php');
 }
 
 $CLASS = new $CONTROLLER;
@@ -33,6 +32,3 @@ $CLASS->$ACTION();
 
 
 
-
-
-?>
